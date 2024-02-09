f075bda835d5ec4f651010a24836b23e3206dbb9
santinofernandez14 <141685479+santinofernandez14@users.noreply.github.com>
2024-02-08 21:00:02 -0300

    cambios

---
+++ clase17/app.js
@@ -0,0 +1,122 @@
+/*let miFuncion=function sumar(a,b){
+    return a+b 
+}
+
+
+
+
+//lineas mas abajo
+
+let resultado=sumar(20,15)
+console.log('la suma de los numeros es '+resultado) 
+
+
+function algo(funcparam,dato){
+    let res= funcparam(dato)
+    console.log('resultado '+res)
+}
+
+function tarea(a){
+    return 2*a
+}
+
+
+algo(tarea,90)*/
+
+
+/*function sumar(){
+    let total=0
+    for(let i=0; i<arguments.length;i++){
+        total+=arguments
+
+    }
+    return total
+}
+
+
+let res= sumar(20,33,45,10)
+console.log('resultado '+res)*/
+
+/*lista=['uno','dos'];
+
+
+//agregar al final
+lista.push("ultimo");
+
+//agregar adelante
+lista.unshift("primero");
+
+
+//eliminar el ultimo elemento
+lista.pop();
+
+//eliminar el primer elemento
+lista.shift();
+
+//clonar
+let otraLista=lista.slice();
+
+//concatenar (unir)
+let diferente=["tres","cuatro"];
+
+let otro=lista.concat(diferente);
+
+//Cambiar elementos.
+otro.fill("x",1,2);
+
+//Obtener la posicion de un elemento.
+let indice=otro.indexOf('cinco');
+console.log("Posicion "+indice);
+
+
+console.log(lista);
+console.log(otraLista);
+console.log(otro);
+
+
+//ordenar
+
+let arreglo=[12,3,9,8,2];
+
+console.log('ordenar '+ arreglo.sort(a,b=>a-b));*/
+
+
+
+
+/*let persona={
+
+    nombre:"Juan",
+    apellido:"Perez",
+    edad:50,
+    fechNac:new Date('1973-12-20'),
+    
+    mostrarInfo: function (){ console.log(this.nombre +" "+this.apellido+" "+this.edad); }
+
+
+}
+
+
+console.log("Edad "+persona.edad);
+console.log("Edad "+persona['edad']);
+persona.mostrarInfo();*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
